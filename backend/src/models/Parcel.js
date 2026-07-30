3c90aac9bc6e2a822ae0f52da39ba39155a5cd2c
premkathayatpk <premkathayat13@gmail.com>
2026-07-30 23:01:20 +0545

    add parcel model

---
+++ backend/src/models/Parcel.js
@@ -0,0 +1,94 @@
+import mongoose from "mongoose";
+
+const parcelSchema = new mongoose.Schema(
+  {
+    customer: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    driver: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      default: null,
+    },
+    pickupContact: {
+      name: {
+        type: String,
+        required: true,
+        trim: true,
+      },
+      phone: {
+        type: String,
+        required: true,
+        trim: true,
+      },
+    },
+    deliveryContact: {
+      name: {
+        type: String,
+        required: true,
+        trim: true,
+      },
+      phone: {
+        type: String,
+        required: true,
+        trim: true,
+      },
+    },
+    parcelType: {
+      type: String,
+      enum: [
+        "Document",
+        "Electronics",
+        "Clothes",
+        "Food",
+        "Fragile",
+        "Medicine",
+        "Other",
+      ],
+      default: other,
+    },
+    weight: {
+      type: Number,
+      required: true,
+      min: 0.1,
+    },
+    price: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    status: {
+      type: String,
+      enum: [
+        "Pending",
+        "Assigned",
+        "Picked Up",
+        "In Transit",
+        "Delivered",
+        "Cancelled",
+      ],
+      default: panding,
+    },
+    pickedUpAt: {
+      type: Date,
+    },
+
+    deliveredAt: {
+      type: Date,
+    },
+    notes: {
+      type: String,
+      default: "",
+      trim: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const Parcel = mongoose.model("Parcel", parcelSchema);
+
+export default Parcel;
